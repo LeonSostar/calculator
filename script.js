371b87dfd0e3b76b12adddf984c3bac43296c655
Leon Sostar <sostarleon@gmail.com>
2024-04-29 16:15:45 +0200

    Initial fully working version

--- script.js
+++ script.js
@@ -1,23 +1,86 @@
-let firstNum = 0;
-let secondNum = 0;
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let equation = '';
+let result;
+const numArr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const funcArr = ['add', 'subtract', 'multiply', 'divide'];
 
-function add(a, b)  {
+const inputField = document.querySelector('#result');
+const elements = document.querySelector('#functions');
+
+elements.addEventListener('click', (e) => {
+    let valE = e.target;
+    if (numArr.includes(valE.id)) {
+        if (!operator) {
+            firstNum += valE.textContent;
+            equation += valE.textContent;
+            inputField.textContent = equation;
+        }
+        else {
+            secondNum += valE.textContent;
+            equation += valE.textContent;
+            inputField.textContent = equation;
+        }
+    }
+    else if (funcArr.includes(valE.id)) {
+        if (!operator) {
+            operator = valE.textContent;
+            equation += ' ' + valE.textContent + ' ';
+            inputField.textContent = equation;
+        }
+        else {
+            inputField.textContent = equation;
+        }
+    }
+    else if (valE.id == 'clear') {
+        equation = '';
+        firstNum = '';
+        operator = '';
+        secondNum = '';
+        inputField.textContent = equation;
+    }
+    else if (valE.id == 'equal') {
+        if (operator == '/' && secondNum == '0') {
+            inputField.textContent = 'DO NOT';
+        }
+        else if (firstNum && operator && secondNum) {
+            result = operate(Number(firstNum), Number(secondNum), operator);
+            firstNum = Number(result);
+            equation = firstNum + '';
+            inputField.textContent = firstNum;
+            console.log(firstNum);
+            operator = '';
+            secondNum = '';
+        }
+    }
+})
+
+function add(a, b) {
     return Math.round((a + b) * 100) / 100;
 }
 
-function subtract(a, b)  {
+function subtract(a, b) {
     return Math.round((a - b) * 100) / 100;
 }
 
-function multiply(a, b)  {
+function multiply(a, b) {
     return Math.round((a * b) * 100) / 100;
 }
 
-function divide(a, b)  {
+function divide(a, b) {
     return Math.round((a / b) * 100) / 100;
 }
 
-console.log(add(2.32432324, 4));
-console.log(subtract(2.32432324, 4));
-console.log(multiply(2, 4));
-console.log(divide(9, 3.5));
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
